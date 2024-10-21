d4ae0b36abccb31e1cf2db454193107f8b938a5e
Svitlana Grytsai <34807666+SvitlanaG@users.noreply.github.com>
2024-10-21 18:42:37 +0300

    feat: implement errors on the server side

--- src/server.ts
+++ src/server.ts
@@ -14,87 +14,105 @@ const server = http.createServer(
     const userId = urlParts?.[3];
     const controller = new Controller();
 
-    if (request.url === '/api/users' && request.method === 'GET') {
-      const users = await controller.getUsers();
-      response.writeHead(200, { 'Content-Type': 'application/json' });
-      response.write(JSON.stringify(users));
-      response.end();
-    } else if (request.url?.startsWith('/api/users/') && request.method === 'GET') {
-      if (!isValidUUID(userId || '')) {
-        response.writeHead(400, { 'Content-Type': 'application/json' });
-        response.write(JSON.stringify({ error: 'Invalid user ID' }));
-        return response.end();
-      }
-      try {
-        const user = await controller.getUser(userId as string);
+    try {
+      if (request.url === '/api/users' && request.method === 'GET') {
+        const users = await controller.getUsers();
         response.writeHead(200, { 'Content-Type': 'application/json' });
-        response.write(JSON.stringify(user));
-      } catch (error) {
-        response.writeHead(404, { 'Content-Type': 'application/json' });
-        response.write(JSON.stringify({ error }));
-      } finally {
+        response.write(JSON.stringify(users));
         response.end();
-      }
-    } else if (request.url === '/api/users' && request.method === 'POST') {
-      try {
+      } else if (
+        request.url?.startsWith('/api/users/') &&
+        request.method === 'GET'
+      ) {
+        if (!isValidUUID(userId || '')) {
+          response.writeHead(400, { 'Content-Type': 'application/json' });
+          response.write(JSON.stringify({ error: 'Invalid user ID' }));
+          return response.end();
+        }
+
+        try {
+          const user = await controller.getUser(userId as string);
+          response.writeHead(200, { 'Content-Type': 'application/json' });
+          response.write(JSON.stringify(user));
+        } catch (error) {
+          response.writeHead(404, { 'Content-Type': 'application/json' });
+          response.write(
+            JSON.stringify({ error: `User with id ${userId} not found!` }),
+          );
+        } finally {
+          response.end();
+        }
+      } else if (request.url === '/api/users' && request.method === 'POST') {
         const body = await getRequestData(request);
         const { username, age, hobbies } = JSON.parse(body);
 
         if (!username || typeof age !== 'number' || !Array.isArray(hobbies)) {
           response.writeHead(400, { 'Content-Type': 'application/json' });
           response.write(JSON.stringify({ error: 'Invalid request body' }));
-          response.end();
-          return;
+          return response.end();
         }
+
         const newUser = await controller.createUser({ username, age, hobbies });
         response.writeHead(201, { 'Content-Type': 'application/json' });
         response.write(JSON.stringify(newUser));
-      } catch (error) {
-        response.writeHead(400, { 'Content-Type': 'application/json' });
-        response.write(JSON.stringify({ error: 'Failed to create user' }));
-      }
-      response.end();
-    } else if (request.url?.startsWith('/api/users/') && request.method === 'PUT') {
-      if (!isValidUUID(userId || '')) {
-        response.writeHead(400, { 'Content-Type': 'application/json' });
-        response.write(JSON.stringify({ error: 'Invalid user ID' }));
-        return response.end();
-      }
-      try {
+        response.end();
+      } else if (
+        request.url?.startsWith('/api/users/') &&
+        request.method === 'PUT'
+      ) {
+        if (!isValidUUID(userId || '')) {
+          response.writeHead(400, { 'Content-Type': 'application/json' });
+          response.write(JSON.stringify({ error: 'Invalid user ID' }));
+          return response.end();
+        }
+
         const body = await getRequestData(request);
         const updatedData = JSON.parse(body);
-        const updatedUser = await controller.updateUser(
-          userId as string,
-          updatedData,
-        );
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        response.write(JSON.stringify(updatedUser));
-      } catch (error) {
-        response.writeHead(404, { 'Content-Type': 'application/json' });
-        response.write(JSON.stringify({ error }));
-      }
-      response.end();
-    } else if (
-      request.url?.startsWith('/api/users/') &&
-      request.method === 'DELETE'
-    ) {
-      if (!isValidUUID(userId || '')) {
-        response.writeHead(400, { 'Content-Type': 'application/json' });
-        response.write(JSON.stringify({ error: 'Invalid user ID' }));
-        return response.end();
-      }
 
-      try {
-        await controller.deleteUser(userId as string);
-        response.writeHead(204);
-      } catch (error) {
+        try {
+          const updatedUser = await controller.updateUser(
+            userId as string,
+            updatedData,
+          );
+          response.writeHead(200, { 'Content-Type': 'application/json' });
+          response.write(JSON.stringify(updatedUser));
+        } catch (error) {
+          response.writeHead(404, { 'Content-Type': 'application/json' });
+          response.write(
+            JSON.stringify({ error: `User with id ${userId} not found!` }),
+          );
+        } finally {
+          response.end();
+        }
+      } else if (
+        request.url?.startsWith('/api/users/') &&
+        request.method === 'DELETE'
+      ) {
+        if (!isValidUUID(userId || '')) {
+          response.writeHead(400, { 'Content-Type': 'application/json' });
+          response.write(JSON.stringify({ error: 'Invalid user ID' }));
+          return response.end();
+        }
+
+        try {
+          await controller.deleteUser(userId as string);
+          response.writeHead(204);
+          response.end();
+        } catch (error) {
+          response.writeHead(404, { 'Content-Type': 'application/json' });
+          response.write(
+            JSON.stringify({ error: `User with id ${userId} not found!` }),
+          );
+          response.end();
+        }
+      } else {
         response.writeHead(404, { 'Content-Type': 'application/json' });
-        response.write(JSON.stringify({ error }));
+        response.write(JSON.stringify({ error: 'Route not found' }));
+        response.end();
       }
-      response.end();
-    } else {
-      response.writeHead(404, { 'Content-Type': 'application/json' });
-      response.write(JSON.stringify({ error: 'Route not Found' }));
+    } catch (error) {
+      response.writeHead(500, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ error: 'Internal server error' }));
       response.end();
     }
   },
